4e348395d5aada68626dd3cf00e50c5cd6264b1e
avinash <polisettyavinash2@gmail.com>
2024-01-01 16:20:16 +0530

    stage1 complieted

--- src/load_data.py
+++ src/load_data.py
@@ -2,7 +2,7 @@ import os
 from get_data import read_params,get_data
 import argparse
 
-
+#loading the data and saving
 def load_and_save(config_path):
     config=read_params(config_path)
     df=get_data(config_path)
