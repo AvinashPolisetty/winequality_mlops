71ec93b9dbad90672d550f1f100ac30a143efb14
avinash <polisettyavinash2@gmail.com>
2024-01-01 19:44:37 +0530

    added test

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+
+setup(
+    name="An mlops project",
+    version="0.0.0",
+    author_email="aviavinashp64@gmail.com",
+    author="avinash",
+    packages=find_packages(),
+    
+)
